56d63046503a91f9fb031d05f05c2dc8b6f15d19
woodstock1993 <diqkqkqk@gmail.com>
2022-07-21 21:34:57 +0900

    (add)자동화 케이스2 주석 추가

--- automation_case_2.py
+++ automation_case_2.py
@@ -2,8 +2,6 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from time import sleep
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-
 import os
 
 url = "https://explorer.kstadium.io/"
@@ -12,15 +10,12 @@ one_exist = False
 number_exist = False
 
 
-# 드라이버 설정
-def set_chrome_driver():
-    chrome_options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-    return driver
-
-
-# 인자로 현재 경로에 덧붙일 경로를 넣는다. 폴더 경로를 덧붙여 설정해주어야 한다.
 def create_folder(added_path):
+    """
+    :param added_path: 현재 경로에 폴더(/폴더이름)을 포함한 경로를 추가로 적어준다.
+    :function: 덧붙인 경로에 폴더를 생성한다. 같은 이름이 있으면 폴더를 생성하지 않는다.
+    :return: 해당 폴더가 위치한 경로를 반환한다.
+    """
     current_path = os.getcwd()
     path = current_path + added_path
     if not os.path.exists(path):
@@ -29,8 +24,11 @@ def create_folder(added_path):
     return path
 
 
-# 최신파일 번호를 리턴한다. 번호로 된 파일이 없으면 새 시작을 의미하는 1을 리턴한다.
 def return_latest_jpg_number(path_dir):
+    """
+    :param path_dir: 현재 경로에 덧붙일 폴더(/폴더이름)을 포함한 경로를 적어준다.
+    :return: 폴더 내에 숫자만으로 이루어진 이름이 있을 경우 그 중에서 가장 큰 번호를 반환한다. 없을 경우 1을 반환한다.
+    """
     global one_exist
     global number_exist
     num_arr = []
@@ -59,7 +57,20 @@ def return_latest_jpg_number(path_dir):
     return 1
 
 
-folder_path = create_folder('/case_2_screen_shot')
+"""
+    - 현재 경로에 /automation_case_2 폴더 생성
+    - 드라이버 설정
+    - automation_case_2 폴더 내 파일 번호 반환
+    - global 변수
+        - cur_page: 접속한 url 페이지를 나타내는 번호
+        - enter: 사용자 함수가 한번 실행 됐음을 체크하는 bool
+        - enter_2: 사용자 함수 내 재귀함수가 실행 됐음을 체크하는 bool
+        - transaction_check: 1이 나왔음을 체크하는 bool
+        - block_height_css_selector: 접속한 페이지 내 tag를 불러오기 위한 선택자
+        - click_x_path: 트랜잭션 값이 1이 나오지 않았을 때 다음 페이지로 넘어가기 위해 필요한 선택자  
+"""
+
+folder_path = create_folder('/automation_case_2')
 chromedriver_path = os.getcwd() + '/chromedriver'
 driver = webdriver.Chrome(service=Service(f'{chromedriver_path}'), options=webdriver.ChromeOptions())
 png_num = return_latest_jpg_number(folder_path)
@@ -74,6 +85,11 @@ click_x_path = '//*[@id="root"]/div[2]/main/section[2]/div/div[2]/div/span[2]/a'
 
 
 def search_block_height(cur_url):
+    """
+    :param cur_url: 접속할 페이지 url
+    :function: block num 의 transaction 이 1이 나타날 때까지 다음 페이지로 이동하면서 방문한 페이지의 화면을 스크린샷으로 찍어 지정한 폴더 경로에 저장한다.
+    :return: None
+    """
     global driver
     global folder_path
     global png_num
@@ -108,7 +124,7 @@ def search_block_height(cur_url):
                 if transaction == 1:
                     transaction_check = True
             except ValueError:
-                print(f'block_height or transaction이 숫자형태가 아닙니다')
+                print(f'block_height or transaction 이 숫자형태가 아닙니다')
                 continue
     if transaction_check is True and one_exist is False and png_num == 1:
         driver.save_screenshot(screen_image_name)
@@ -147,6 +163,10 @@ def search_block_height(cur_url):
 
 
 def print_result():
+    """
+    :function: transaction 1을 찾기 위해 search_block_height 함수가 돈 횟수를 출력한다.
+    :return: None
+    """
     global cur_page
     global enter
     global enter_2
