ba12aa01931bafb5bd2da2669c925b93f0faf6ef
woodstock1993 <diqkqkqk@gmail.com>
2022-07-24 16:55:24 +0900

    (fix) 자동화 케이스 코드 수정

--- automation_case_1.py
+++ automation_case_1.py
@@ -11,20 +11,6 @@ chromedriver_path = os.getcwd() + '/chromedriver'
 driver = webdriver.Chrome(service=Service(chromedriver_path), options=webdriver.ChromeOptions())
 
 
-def create_folder(added_path):
-    """
-    :param added_path: 현재 경로에 폴더(/폴더이름)을 포함한 경로를 추가로 적어준다.
-    :function: 덧붙인 경로에 폴더를 생성한다. 같은 이름이 있으면 폴더를 생성하지 않는다.
-    :return: 해당 폴더가 위치한 경로를 반환한다.
-    """
-    current_path = os.getcwd()
-    path = current_path + added_path
-    if not os.path.exists(path):
-        os.mkdir(path)
-        print(f'{path} 경로에 폴더가 생성되었습니다')
-    return path
-
-
 def trim_data(data, block_num):
     """
     :param data: block 정보를 가진 dict 형태의 데이터
